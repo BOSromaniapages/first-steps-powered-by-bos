acdbc1f9ae7aa595846f68fe4973fadcc3f2d006
r3kyfx <rakiufx@gmail.com>
2025-10-27 21:14:47 +0200

    Update site

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/first-steps-powered-by-bos",
+  base: '/first-steps-powered-by-bos/',
 })
